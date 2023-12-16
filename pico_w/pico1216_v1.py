df1f9b156c81a00f47dcb5e2d47fa50211e89250
Miunros <nancy42043@gmail.com>
2023-12-16 10:52:02 +0800

    led test

--- pico_w/pico1216_v1.py
+++ pico_w/pico1216_v1.py
@@ -3,6 +3,6 @@ from machine import Pin
 led = Pin("LED", Pin.OUT)
 while True:
     led.value(1)
-    time.sleep(1)  #表示在這裡停1秒不要動
+    time.sleep_ms(500)  #表示在這裡停1秒不要動
     led.value(0)
     time.sleep(1)	#繼續執行
