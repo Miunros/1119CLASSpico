c56932d84373369aaa5f8eb49263f5b0843fcbef
Miunros <nancy42043@gmail.com>
2024-01-20 09:32:43 +0800

    modify

--- pico_w/pico0106_v2.py
+++ pico_w/pico0106_v2.py
@@ -1,36 +1,33 @@
-from tools import connect, reconnect 
-from machine import Timer, WDT, ADC,RTC
-import network
-import time
 import urequests as requests
+from tools import connect,reconnect
+from machine import WDT,Timer,ADC,RTC
+import time
 
-
-def mycallback(t):
-    print('Hello World!')
-    t.deinit()
 
 def alert(t:float):
-    print('要爆炸了!!!')
-    rtc= RTC()
-    date_time=rtc.datetime()
-    year = date_time[0]
-    month = date_time[1]
-    day = date_time[2]
-    hour = date_time[4]
-    minites = date_time[5]
-    second = date_time[6]
-    date_str=f'{year}-{month}-{day}-{hour}:{minites}:{second}'
+    print('要爆炸了!')
+    rtc = RTC()
+    date_tuple = rtc.datetime()
+    year = date_tuple[0]
+    month = date_tuple[1]
+    day = date_tuple[2]
+    hour = date_tuple[4]
+    minites = date_tuple[5]
+    second = date_tuple[6]
+    date_str = f'{year}-{month}-{day} {hour}:{minites}:{second}'
+    get_url = f'https://hook.us1.make.com/tw20asr31id34dhmjlaf1usu162hw758?name=我家雞場&date={date_str}&temperature={t}'
     try:
-        res = requests.get(f'https://hook.us1.make.com/tw20asr31id34dhmjlaf1usu162hw758?name=台北&date={date_str}&temperature={t}&place=Taipei')
+        response = requests.get(get_url)
     except:
-        reconnect()        
+        reconnect()
     else:
-        if reconnect.status_code==200:
-            print('傳送成功')
+        if response.status_code == 200:
+            print("傳送成功")
         else:
-            print('Server有錯誤訊息')
+            print("server有錯誤訊息")
             print(f'status_code:{response.status_code}')
         response.close()
+    
     
 def callback1(t:Timer):
     global start
@@ -41,15 +38,13 @@ def callback1(t:Timer):
     delta = time.ticks_diff(time.ticks_ms(), start)
     print(delta)
     #溫度超過24度,並且發送alert()的時間已經大於60秒
-    if temperature >= 25 and delta >= 60 * 1000:        
+    if temperature >= 24 and delta >= 60 * 1000:        
         alert(temperature)
         start = time.ticks_ms()#重新設定計時的時間
+        
 
-connect()
-start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒
+connect()       
+
+start = time.ticks_ms() - 60 * 1000 #應用程式啟動時,計時時間,先減60秒    
 time1 = Timer()
 time1.init(period=1000,callback=callback1)
-
-#https://hook.us1.make.com/tw20asr31id34dhmjlaf1usu162hw758
-
-
--- pico_w/tools.py
+++ pico_w/tools.py
@@ -1,41 +1,60 @@
-from machine import Timer, WDT, ADC,RTC
 import network
-import time
 import urequests as requests
+import time
+import rp2
+from machine import WDT
+
+rp2.country('TW')
+
 
 ssid = 'NancyHsu'
-password='22222222'
+password = '22222222'
 
-nic = network.WLAN(network.STA_IF)
-nic.active(True)
-nic.connect(ssid, password)
-nic.config=(pm=0xa11140)
 
-def connect():
-    #處以正在連線vvvvvvvvvvvvv
-    max_wait=10 #建立變數
-    while max_wait>0: #比較變數
-        max_wait -=1  #每秒相減1，直到max_wait<=0才會跳出
-        status = nic.status()
-        if status >=3 or status <0: #大於等於3表示錯誤訊息；<0表示沒有主機
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+wlan.config(pm = 0xa11140)
+
+def connect():  
+
+    #等待連線或失敗
+    #status=0,1,2正在連線
+    #status=3連線成功
+    #<1,>=3失敗的連線
+    max_wait = 10    
+
+    while max_wait > 0:
+        status = wlan.status()
+        if status < 0 or status >= 3:
             break
-        print("等待連線中")
+        max_wait -= 1
+        print("等待連線")
         time.sleep(1)
-    #可能完全沒有連線(沒有wifi機)
-    if nic.status() != 3:
-        #開發狀態底下不使用重新開機的功能wdt = WDT(timeout=2000)
-        #實際產品化再重新開機wdt.feed()
-        raise RuntimeError('連線失敗')
-    
+
+    #處理錯誤
+    if wlan.status() != 3:
+        print('連線失敗,重新開機')
+        #raise RuntimeError('連線失敗')
+        wdt = WDT(timeout=2000)
+        wdt.feed()
     else:
-        print("連線成功")
-        print (nic.ifconfig())
+        print('連線成功')
+        status = wlan.ifconfig()
+        print(f'ip={status[0]}') 
+        
+        
 def reconnect():
-    if nic.status()==3:
-        print('無法連線({nic.status()})')
+    if wlan.status() == 3: #還在連線,只是傳送的server無回應
+        print(f"無法連線({wlan.status()})")
         return
     else:
-        print('嘗試重新連線')
-        nic.disconnect()
-        nic.connect(ssid, password)
+        print("嘗試重新連線")
+        wlan.disconnect()
+        wlan.connect(ssid, password)
         connect()
+        
+        
+
+        
