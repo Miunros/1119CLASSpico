559359b0bbcbe0319299d4d9f3129f6e5bc53b7f
Miunros <nancy42043@gmail.com>
2024-01-20 12:03:19 +0800

    按按鈕亮燈

--- pico_w/pico0120_v1.py
+++ pico_w/pico0120_v1.py
@@ -2,12 +2,21 @@ from machine import Pin
 import time
 led_red = Pin(15, mode=Pin.OUT)
 button_red = Pin(14, mode=Pin.PULL_DOWN)
+is_press = False
 
 while True:
     #time.sleep_ms(500) = 0.5秒 (1000)=1秒
-    print(button_red.value())
-    time.sleep_ms(500)
-    
+    #time.sleep_ms(500)
+    #數值1代表True
+    if button_red.value():
+        is_press = True
+        led_red.value(1)
+    elif is_press:
+        print('釋放')
+        is_press = False
+        led_red.value(0)
+
+
 #led_red.off() > 關燈
 #led_red.on() > 開燈
 #led_red.value(0) > 關燈
