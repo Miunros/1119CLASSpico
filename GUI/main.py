cad384bb526de0243db320e1f0953f3875f7aa13
Miunros <nancy42043@gmail.com>
2024-02-03 03:27:16 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 #st.write('Hello, Nancy :sunglasses:')
 #st.caption('我是許乃心')
-st.title('我家的雞舍')
+st.title('群輝商務科技的雞舍')
 st.header(':blue[pico專案1] :red[溫度] :violet[濕度]',divider='rainbow')
