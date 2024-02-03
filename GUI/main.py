8d96e2a62436adbeffbcf4958c49ec39efdc7f40
Miunros <nancy42043@gmail.com>
2024-02-03 07:45:08 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import time
 import requests
 from streamlit_autorefresh import st_autorefresh
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 #st.write('Hello, Nancy :sunglasses:')
 #st.caption('我是許乃心')
 st.title('群輝商務科技的雞舍')
@@ -15,7 +17,7 @@ count = st_autorefresh(interval=5000, limit=100, key="fizzbuzzcounter")
 #    time.sleep(3)
 #st.success('Done!')
 
-url='https://blynk.cloud/external/api/get?token=OjsaRz73dEbe7wlmUomfBRB38u8kSMj6&v1&v0'
+url=f'https://blynk.cloud/external/api/get?token={os.environ["API_KEY"]}&v1&v0'
 
 req = requests.request('GET',url)
 if req.status_code ==200:
