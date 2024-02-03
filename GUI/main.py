8a84eae7a2c911300318965e2cba78e122d00e31
Miunros <nancy42043@gmail.com>
2024-02-03 03:04:22 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Hello, *World!* :sunglasses:')
+#st.write('Hello, Nancy :sunglasses:')
+#st.caption('我是許乃心')
+st.title('我家的雞舍')
+st.header(':blue[pico專案1] :red[溫度] :violet[濕度]',divider='rainbow')
